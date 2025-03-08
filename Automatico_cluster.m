fbd6068f3c062f58caf12ec081e571c984f5ecde
Pmagrinya <paula.magrinya@uam.es>
2025-03-08 13:50:34 +0100

    Automatico_cluster.m

--- Automatico_cluster.m
+++ Automatico_cluster.m
@@ -19,7 +19,13 @@ fnamestring = char([]);
 dname       = char([]);
 
 % Specify the number of videos (image sequences) you want to analyze.
-nv = 1;  
+prompt = {'Enter number of videos to analyze:'};
+dlgtitle = 'Number of Videos';
+dims = [1 40];   
+definput = {'1'};  
+answer = inputdlg(prompt, dlgtitle, dims, definput);
+
+nv = str2double(answer{1}); 
 
 % ===================================================================
 % LOOP 1: Acquires input for each video sequence (folder of images) 
